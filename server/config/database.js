c0de083e7e79f84e8822921c7c6c20d68d20cadc
Pratiti Paul <paulpratiti23@gmail.com>
2026-06-26 12:15:33 +0530

    changes

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,28 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
     await mongoose.connect(process.env.MONGODB_URI);
+
     console.log("MongoDB Connected");
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
+  } catch (err) {
+    console.log(err);
   }
 };
 
